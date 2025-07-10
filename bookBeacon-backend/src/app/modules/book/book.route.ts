b965e3f4eb105d623ab87bc54f7726ea613ec805
Mahdi Hassan <mahdioffi333@gmail.com>
2025-07-10 19:25:17 +0600

    added routes.ts file

---
+++ bookBeacon-backend/src/app/modules/book/book.route.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import { createBook, deleteBook, findAllBooks, findSingleBook, updateBook } from "./book.controller";
+
+const router = express.Router();
+
+router.route("/").get(findAllBooks);
+router.route("/:bookId").get(findSingleBook);
+router.route("/").post(createBook);
+router.route("/:bookId").put(updateBook);
+router.route("/:bookId").delete(deleteBook);
+
+export default router;
