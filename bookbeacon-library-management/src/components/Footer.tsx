d301cec4995110acb06211669f0a1530a0ba32fa
Mahdi Hassan <mahdioffi333@gmail.com>
2025-07-13 18:29:02 +0600

    added footer components

---
+++ bookbeacon-library-management/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer className="bg-primary-light dark:bg-primary-dark text-white p-4 text-center">
+      <p>© Copyright {new Date().getFullYear()} BookBeacon Library management. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
